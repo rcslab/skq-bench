38eb051eb9156a52f21d61ac20f198d902de39d7
Haoyu Gu <guhaoyu2005@gmail.com>
2019-04-06 20:07:18 -0400

    Modified plot thing
    Test Plan: test
    
    Reviewers: oscar
    
    Reviewed By: oscar
    
    Differential Revision: https://review.rcs.uwaterloo.ca/D80

--- graph/connection_scale.py
+++ graph/connection_scale.py
@@ -4,16 +4,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import re
 
 if len(sys.argv) > 1:
     df = pd.read_csv(sys.argv[1], skipinitialspace=True)
+    if len(sys.argv) == 3:
+        if sys.argv[1][len(sys.argv[1])-4: 4] == '.csv':
+            fn = re.match(r'(^.+)\.csv$', sys.argv[1]).group(1)
+        else:
+            fn = sys.argv[1]
+
+        if sys.argv[2] == '.':
+            output_img_fn = fn + '.png'
+        else:
+            output_img_fn = sys.argv[2]
 else:
     df = pd.read_csv("test.csv", skipinitialspace=True)
 
 print(df)
-
 kq = df['KQueue Count'].unique()
 threads = df['Threads Count(Server)'].unique()
+
+plt.figure(1)
 
 for k in kq:
     kdf = df.loc[df['KQueue Count'] == k]
@@ -35,6 +47,9 @@ for k in kq:
 plt.title('Connection Scalability')
 plt.xlabel('Connections')
 plt.ylabel('Events Per Second')
+plt.gcf().set_size_inches(30, 20)
 
 plt.legend(loc='best')
+if len(sys.argv) > 2:
+    plt.savefig(output_img_fn)
 plt.show()
