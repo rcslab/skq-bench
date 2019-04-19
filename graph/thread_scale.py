dbd0af1d541468f2feef4147c8a8ee36a8647a9f
BuildTools <unconfigured@null.spigotmc.org>
2019-04-19 15:51:06 -0400

    change thread_scale
    Summary: title
    
    Test Plan: lol
    
    Reviewers: ali
    
    Reviewed By: ali
    
    Differential Revision: https://review.rcs.uwaterloo.ca/D89

--- graph/thread_scale.py
+++ graph/thread_scale.py
@@ -4,28 +4,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
-import re
 
 if len(sys.argv) > 1:
     df = pd.read_csv(sys.argv[1], skipinitialspace=True)
-    if len(sys.argv) == 3:
-        if sys.argv[1][len(sys.argv[1])-4: 4] == '.csv':
-            fn = re.match(r'(^.+)\.csv$', sys.argv[1]).group(1)
-        else:
-            fn = sys.argv[1]
-
-        if sys.argv[2] == '.':
-            output_img_fn = fn + '.png'
-        else:
-            output_img_fn = sys.argv[2]
 else:
     df = pd.read_csv("test.csv", skipinitialspace=True)
 print(df)
 
 kq = df['KQueue Count'].unique()
 conns = df['Connections Count'].unique()
-
-plt.figure(1)
 
 for k in kq:
     kdf = df.loc[df['KQueue Count'] == k]
@@ -47,10 +34,6 @@ for k in kq:
 plt.title('Thread Scalability')
 plt.xlabel('Threads')
 plt.ylabel('Events Per Second')
-plt.gcf().set_size_inches(30, 20)
 
 plt.legend(loc='best')
-if len(sys.argv) > 2:
-    plt.savefig(output_img_fn)
-plt.show()
-
+plt.savefig(sys.argv[2], format='png', dpi=1200)
